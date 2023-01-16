151457ee70c85567a3947559a0c7562e26ef8ded
kzhanbolat <109128207+kzhanbolat@users.noreply.github.com>
2023-01-16 22:47:03 +0600

    Add files via upload

---
+++ nfactorial-project_1-15_01_2023/index.js
@@ -0,0 +1,24 @@
+const customerName = "AZh";
+const cAddress = "Almaty";
+
+const pickle = {
+  id: 1,
+  price: 500,
+  name: "pickle",
+  type: "vegetable",
+  eatable: true,
+  amount: 4,
+};
+
+const apple = {
+  id: 2,
+  price: 1250,
+  name: "apple",
+  type: "fruit",
+  eatable: true,
+  amount: 1
+};
+
+
+let merged = {pickle, apple};
+const allRules = {pickle, pple};
